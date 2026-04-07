28587758cf3d49f8866418e5eebe346d90c52a78
anmire <areffelt@plugpower.com>
2026-04-07 09:54:56 -0400

    v4.4.0: Fix Sudoku (easier difficulty, pencil mode, number highlighting, numpad greying) + tutorials for all 11 brain exercise games

---
+++ calc/pkg/amni_calc_wasm_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const bolt_tables: (a: number) => void;
+export const calc_beam: (a: number, b: number, c: number) => void;
+export const calc_bolt: (a: number, b: number, c: number) => void;
+export const calc_fatigue: (a: number, b: number, c: number) => void;
+export const calc_seal: (a: number, b: number, c: number) => void;
+export const calc_seal_groove: (a: number, b: number, c: number) => void;
+export const calc_section: (a: number, b: number, c: number) => void;
+export const calc_spring: (a: number, b: number, c: number) => void;
+export const calc_stress: (a: number, b: number, c: number) => void;
+export const calc_thermal: (a: number, b: number, c: number) => void;
+export const convert_unit: (a: number, b: number, c: number, d: number, e: number) => number;
+export const get_finishes: (a: number) => void;
+export const get_material: (a: number, b: number, c: number) => void;
+export const get_materials: (a: number) => void;
+export const recommend_finishes: (a: number, b: number, c: number) => void;
+export const seal_materials: (a: number) => void;
+export const seal_standard_sizes: (a: number) => void;
+export const section_presets: (a: number) => void;
+export const unit_categories: (a: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_export: (a: number, b: number, c: number) => void;
+export const __wbindgen_export2: (a: number, b: number) => number;
+export const __wbindgen_export3: (a: number, b: number, c: number, d: number) => number;
